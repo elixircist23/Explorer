3c77b2eb9950a208599bee9ced84646ded7fb92a
tbone255 <taha.amir13971@gmail.com>
2015-08-18 16:17:41 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This program will be a file explorer and a ftp application
+"""
+
+#importing some kush
+import os
+import time
+#mm so sour on hamburgers
+import pickle
+from Tkinter import *
+
+
+
+#some gui stuff
+root = Tk()
+label = Label(root, text ="ayy lmao")
+label.pack()
+
+root.mainloop()
