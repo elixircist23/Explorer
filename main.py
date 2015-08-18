68cd815aff355f9e406db75b3dacc437fbace8b4
elixircist23 <elixircist@gmail.com>
2015-08-18 17:09:42 -0400

    list directory update
    lists directories into frame, try to make listbox

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from tkinter import *
+from tkinter import *;
+import os;
 
 class Explorer(Frame):
 	def __init__(self, parent):
-		Frame.__init__(self, parent, background = "white")
-		self.parent = parent
-		self.parent.title("Explorer")
-		self.pack(fill=BOTH, expand=1)
+		Frame.__init__(self, parent, background = "white");
+		self.parent = parent;
+		
+		self.userID = os.getlogin();
+		self.startDir = "C:/Users/" + self.userID;
+		self.listDir = os.listdir(self.startDir);
+		self.listDirLength = len(self.listDir);
+		
+	def initUI(self):
+		self.parent.title("Explorer");
+		self.pack(fill=BOTH, expand=1);
+		
+		listBox = Listbox(self);
+		for i in listDir:
+			listBox.insert(END, i);
+		
+		#listBox.bind("<<ListboxSelect>>", "method");
+		
+	
 		
 def main():
-	root = Tk()
-	root.geometry("250x150+300+300")
+	root = Tk();
+	root.geometry("250x150+300+300");
 	app = Explorer(root)
 	root.mainloop()
 
 if __name__ == '__main__':
 	main();
-	
-	
+
