8a30d225b0b78643336029ef465c2ae5f5f6f72f
elixircist23 <elixircist@gmail.com>
2015-08-19 16:22:38 -0400

    forward working explorer
    need back function

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class Explorer(Frame):
 		self.currentDir = "C:/Users/" + os.getlogin();
 		os.chdir(self.currentDir);
 		
+		self.dirList = self.listDir();
+		
 		#call constructor from Frame, create parent to reference object
 		Frame.__init__(self, parent, background = "white");
 		self.parent = parent;
@@ -27,13 +29,13 @@ class Explorer(Frame):
 		#expand frame both directions
 		self.pack(fill=BOTH, expand = 1);
 		
-		#create listbox that hold all directories in the current directory
-		dirList = get
-		lb = Listbox(self)
-		for i in self.listDir():
-			lb.insert(END, i);
-
-		lb.pack(side = RIGHT, fill=BOTH, expand=1, padx = 10, pady = 5);
+		#create listbox that holds all directories in the current directory
+		self.lb = Listbox(self)
+		for i in self.dirList:
+			self.lb.insert(END, i);
+	
+		self.lb.bind("<<ListboxSelect>>", self.onselect);
+		self.lb.pack(side = RIGHT, fill=BOTH, expand=1, padx = 10, pady = 5);
 		
 	#returns current working directory	
 	def getCurrentDir(self):
@@ -42,18 +44,47 @@ class Explorer(Frame):
 	
 	#changes directory path
 	def setCurrentDir(self, path):
-		self.currentDir = os.chdir(path);
+		os.chdir(path);
+		self.currentDir = os.getcwd()
 	
 	#returns a list of all directories and files in the current directory
 	def listDir(self):
 		return os.listdir(self.currentDir);
-
+	
+	#when listbox element is clicked
+	def onselect(self, evt):
+		
+		#get index and value of clicked item
+		w = evt.widget
+		index = int(w.curselection()[0]);
+		value = w.get(index)
+		
+		#set new path and pass it to 'setCurrentDir', that will move us to the dir, and change self.currentDir to path
+		path = self.currentDir + '/' + value
+		self.setCurrentDir(path);
+		
+		#destroy current lb to make way for the new one
+		self.lb.destroy();
+		
+		#create new lb, but update first
+		self.updateList();
+		self.lb = Listbox(self)
+		for i in self.dirList:
+			self.lb.insert(END, i);
+		
+		self.lb.bind("<<ListboxSelect>>", self.onselect);
+		self.lb.pack(side = RIGHT, fill=BOTH, expand=1, padx = 10, pady = 5);
+		
+	#update the list
+	def updateList(self):
+		self.dirList = self.listDir();
+		
 def main():
 	
 	#create tk object
 	root = Tk();
 	#set dimesions of window
-	#root.geometry("650x500+250+100");
+	root.geometry("650x500+250+100");
 	#pass tk object through our class
 	app = Explorer(root);
 	#enter mainloop
