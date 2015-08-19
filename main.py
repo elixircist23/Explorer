50cdaeda52b24675fa06c0456d5eef505c90f877
tbone255 <taha.amir13971@gmail.com>
2015-08-19 12:05:30 -0400

    main.py
    I made it so when u click a list item, it prints out the files from that folder

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
 from tkinter import *;
 import os;
 
+
 class Explorer(Frame):
+    
     def __init__(self, parent):
-        Frame.__init__(self, parent, background = "white");
-        self.parent = parent;
         
-        self.userID = os.getlogin();
-        self.startDir = "C:/Users/" + self.userID;
-        self.listDir = os.listdir(self.startDir);
-        self.listDirLength = len(self.listDir);
+        Frame.__init__(self, parent, background = "white")
+        self.parent = parent
         
-        self.initUI();
+        self.userID = os.getlogin()
+        self.startDir = "C:/Users/" + self.userID
+        self.listDir = os.listdir(self.startDir)
+        self.listDirLength = len(self.listDir)
         
+        self.initUI()
+
+    def updateDir(self, event):
+        self.item = self.listBox.curselection()
+        print(self.item)
+        self.listDir = os.listdir(self.startDir + "/" + self.listBox.get(self.item[0]))
+        print(self.listDir)
+
+
     def initUI(self):
-        self.parent.title("Explorer");
-        self.pack(fill=BOTH, expand=1);
+        self.parent.title("Explorer")
+        self.pack(fill=BOTH, expand=1)
 
         #FRAMES
         self.topFrame = Frame(self)
-        topFrame.pack(side=TOP)
+        self.topFrame.pack(side=TOP)
         self.bottomFrame = Frame(self)
-        bottomFrame.pack(side=BOTTOM)
+        self.bottomFrame.pack(side=BOTTOM)
         #DIRECTORY SEARCH BAR
-        self.dirLabel = Label(topFrame, text="Current Directory", bg="white")
-        dirLabel.pack(side=LEFT)
+        self.dirLabel = Label(self.topFrame, text="Current Directory", bg="white")
+        self.dirLabel.pack(side=LEFT)
 
-        self.dirEntry = Entry(topFrame)
-        dirEntry.insert(0, self.startDir)
-        dirEntry.pack()
+        self.dirEntry = Entry(self.topFrame)
+        self.dirEntry.insert(0, self.startDir)
+        self.dirEntry.pack()
 
         #LISTBOX
         self.scrollbar = Scrollbar(self)
-        scrollbar.pack(side=RIGHT, fill=Y)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
         
-        self.listBox = Listbox(self, height=20);
+        self.listBox = Listbox(self, height=20, selectmode=SINGLE)
         for i in self.listDir:
-            listBox.insert(END, i);
+            self.listBox.insert(END, i)
 
-        listBox.config(yscrollcommand=scrollbar.set)
-        scrollbar.config(command=listBox.yview)
+        self.listBox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.listBox.yview)
         
-        #listBox.bind("<<ListboxSelect>>", "method");
-        listBox.pack(fill=BOTH, expand=1);
+        self.listBox.bind("<<ListboxSelect>>", self.updateDir)
+        
+
+        self.listBox.pack(fill=BOTH, expand=1)
     
         
 def main():
-    root = Tk();
-    root.geometry("250x150+300+300");
+    root = Tk()
+    root.geometry("250x150+300+300")
     app = Explorer(root)
     root.mainloop()
 
 if __name__ == '__main__':
-    main();
+    main()
